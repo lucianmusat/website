04e03f01501313eeccef262c08c14b4616160d0d
Lucian Musat <lucianmusat@Lucians-MacBook-Pro.local>
2017-06-07 23:55:55 +0200

    Add html editor for the rest of the models.

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 class About(models.Model):
-    content = models.TextField()
+    content = tinymce_models.HTMLField()
--- index/models.py
+++ index/models.py
@@ -1,5 +1,6 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 class Index(models.Model):
-    content = models.TextField()
+    content = tinymce_models.HTMLField()
 
